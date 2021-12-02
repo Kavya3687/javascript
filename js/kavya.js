4aea30f8945ed0debb85000f11a0eb124699f3c9
Kavya3687 <kavyavishwakarma.Xworkz@gmail.com>
2021-12-02 17:09:51 +1100

    js

---
+++ js/kavya.js
@@ -0,0 +1,68 @@
+function Showall(name){
+var input=document.getElementById("name").value;
+var total=input*5;
+document.getElementById("print").innerHTML=total;
+if (name=="lays"){
+alert("lays is of 10rs");
+}
+
+if (name=="kurkure"){
+alert("kurkure is of 20rs");
+console("dairymilk is of 40rs");
+}
+
+
+if (name=="dairymilk"){
+alert("dairymilk is of 40rs");
+console("dairymilk is of 40rs");
+
+}
+if (name=="oreo"){
+alert("oreo is of 30rs");
+console("oreo is of 30rs");
+
+}
+if (name=="bourbon"){
+alert("bourbon is of 20rs");
+console("bourbon is of 20rs");
+
+}
+if (name=="maaza"){
+alert("maaza is of 55rs");
+console("maaza is of 55rs");
+
+}
+if (name=="coke"){
+alert("coke is of 30rs");
+console("coke is of 30rs");
+
+}
+if (name=="parleg"){
+alert("parleG is of 5rs");
+console("parleG is of 5rs");
+
+}
+if (name=="kitkat"){
+alert("kitkat is of 10rs");
+console("kitkat is of 10rs");
+
+}
+
+if (name=="cake"){
+alert("cake is of 80rs");
+console("cake is of 80rs");
+}
+if (name=="jimjam"){
+alert("jimjam is of 80rs");
+console("jimjam is of 80rs");
+}
+
+if (name=="ferrero"){
+alert("ferrero is of 380rs");
+console("ferrero is of 380rs");
+}
+if (name=="badammilk"){
+alert("badammilk is of 45rs");
+console("badammilk is of 45rs");
+}
+}
